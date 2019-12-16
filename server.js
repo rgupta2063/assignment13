8aee16f51955968cbdd4b4df995aee9f5e8570f6
Ritu Gupta <ritugupta@Ghanshyams-MacBook-Air.local>
2019-12-15 21:41:07 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,81 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+// const cors = require('cors')
+const app = express()
+const dbUrl = 'mongodb+srv://Ritu:Ghanshi%401986@cluster0-5m0il.mongodb.net/test?retryWrites=true&w=majority'
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}));
+// app.use(cors())
+app.use(express.static(__dirname))
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    next()
+})
+
+
+const Product = mongoose.model('product',
+     {
+           "productid": Number ,
+           "category": String,
+           "price": Number,
+           "name": String,
+           "instock": Boolean
+    })
+
+app.get('/product/get/', (req, res) =>{
+    Product.find({},(err, products)=>{
+        if (err){
+            console.log('error: ', err)
+        }else{
+            res.send(products)
+        }       
+    })
+})
+
+app.post('/product/create/', (req, res) => {
+    const { id, category, price, name, instock } = req.body
+    let product = new Product({ productId: id, category, price, name, instock })
+  
+    product.save(err => {
+      if (err) {
+        res.sendStatus(500)
+        console.log('Error in saving data: ', err)
+      } else {
+        res.sendStatus(200)
+      }
+    })
+  })
+
+app.put('/product/update/:productId', (req, res) => {
+    Product.updateOne(
+      { productId: req.params.productId },
+      { upsert: false },
+      err => {
+        if (err){ 
+            console.log('Update Error: ', err)
+        }else{ 
+            res.sendStatus(200)}
+      }
+    )
+  })
+
+
+app.delete('/product/delete/:productId', (req, res) => {
+    Product.deleteOne({ productId: req.params.productId }, err => {
+        if (err) {
+            console.log('Delete Error: ', err)
+        }else {
+            res.sendStatus(200)}
+    })
+    res.send(req.params)
+})
+
+
+mongoose.connect(dbUrl,{useNewUrlParser: true, useUnifiedTopology: true } , (err) => {
+    console.log('MongoDb database connection', err)        
+})
+
+app.listen(3001)
---
+++ src/Products.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react'
+import Filters from './Filters'
+import ProductTable from './ProductTable'
+import ProductForm from './ProductForm'
+import $ from 'jquery'
+
+class Products extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            filterText: '',
+            products:[]
+        }
+        this.handleFilter = this.handleFilter.bind(this)
+        this.handleDestroy = this.handleDestroy.bind(this)
+        this.handleSave = this.handleSave.bind(this)
+    }
+
+    componentDidMount() {
+        this.getProducts()
+    }
+    
+    getProducts() {
+        $.ajax({
+          type: "GET",
+          url: 'http://localhost:3001/product/get/'
+        }).then(resp => {
+          console.table(resp)
+          this.setState({
+            products: resp
+          })
+        })
+      }
+
+
+    handleFilter(filterInput) {
+        this.setState(filterInput)
+    }
+
+    handleSave(product) {
+        if (!product.id) {
+            product.id = new Date().getTime()
+        }
+        $.ajax({
+            type: 'POST',
+            url: 'http://localhost:3001/product/create/',
+            data: product
+          }).then(() => {
+            this.getProducts()
+          })
+        }
+    
+    handleDestroy(productId) {
+        $.ajax({
+            type: "DELETE",
+            url: `http://localhost:3001/product/delete/${productId}`
+        }).then(() => {
+            this.getProducts()
+        })
+    }
+    
+    handleUpdateStatus = productId => {
+        $.ajax({
+          type: 'PUT',
+          url: `http://localhost:3001/product/update/${productId}`
+        }).then(() => {
+          this.getProducts()
+        })
+    }
+
+    render () {
+        return (
+            <div>
+                <h1>My Inventory</h1>
+                <Filters 
+                    onFilter={this.handleFilter}></Filters>
+                <ProductTable 
+                    products={this.state.products}
+                    filterText={this.state.filterText}
+                    onDestroy={this.handleDestroy}></ProductTable>
+                <ProductForm
+                    onSave={this.handleSave}></ProductForm>
+            </div>
+        )
+    }   
+}
+
+export default Products
